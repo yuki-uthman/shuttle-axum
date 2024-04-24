6db4b269e665bf3c8d0a78ff31273bf57714b105
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-04-24 15:13:05 +0500

    test: use app address from config file

--- api/lib/tests/server/helpers.rs
+++ api/lib/tests/server/helpers.rs
@@ -1,9 +1,42 @@
 use api_lib::build_router;
+use config::{Config as ConfigCrate, File};
+
+const CONFIG_FILE: &str = "dev.yaml";
+
+#[derive(serde::Deserialize, Debug)]
+struct Application {
+    port: u16,
+    host: String,
+}
+
+impl Application {
+    fn address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Config {
+    pub application: Application,
+}
+
+fn get_config() -> Config {
+    let base_dir = std::env::current_dir().unwrap();
+    let config_path = base_dir.join(CONFIG_FILE);
+    let config = ConfigCrate::builder()
+        .add_source(File::with_name(config_path.to_str().unwrap()))
+        .build()
+        .unwrap();
+
+    config.try_deserialize::<Config>().unwrap()
+}
 
 pub async fn spawn_app() {
+    let config = get_config();
+
     let app = build_router();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+    let listener = tokio::net::TcpListener::bind(config.application.address())
         .await
         .unwrap();
 
