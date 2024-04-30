14399024d69c9761dab4de759aed5e56d7a49348
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-04-30 16:57:12 +0500

    feat: add version endpoint

--- api/lib/src/lib.rs
+++ api/lib/src/lib.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get, Router};
 use sqlx::PgPool;
 
 #[derive(Clone)]
@@ -10,9 +10,23 @@ async fn health() -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
 
+async fn version(State(state): State<AppState>) -> Result<impl IntoResponse, impl IntoResponse> {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(&state.pool)
+        .await;
+
+    println!("{:#?}", result);
+
+    match result {
+        Ok(version) => Ok((StatusCode::OK, version)),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
+    }
+}
+
 pub fn build_router(pool: PgPool) -> Router {
     let state = AppState { pool };
     Router::new()
-        .with_state(state)
         .route("/health", get(health))
+        .route("/version", get(version))
+        .with_state(state)
 }
