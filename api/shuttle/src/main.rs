859928409e282a21c565b6c77e3bd3c69d19a604
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-05-25 11:10:48 +0900

    build: add Secrets.toml

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,8 +1,17 @@
 use api_lib::build_router;
+use shuttle_runtime::SecretStore;
 use sqlx::PgPool;
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_runtime::Secrets] secrets: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    // Secrets.toml
+    // SECRET_KEY = 'this is a secret key'
+    let secret = secrets.get("SECRET_KEY").expect("SECRET_KEY must be set");
+    println!("Secret: {}", secret);
+
     sqlx::migrate!("../../migrations")
         .run(&pool)
         .await
